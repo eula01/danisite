5a563c032e12b64eea83ecc6b850579923da329f
Daniela Dobrinov <dani@MacBook-Air-8.local>
2026-06-12 15:40:43 +0100

    Unlink author names from hyperlinks on links page

--- pages/links.tsx
+++ pages/links.tsx
@@ -7,12 +7,12 @@ export default function Links() {
         <div>
           <p>A random selection of underpriced writings / links</p>
           <ul>
-            <li><a href="https://www.maths.tcd.ie/pub/ims/bull48/M4801.pdf" target="_blank">Real Life Mathematics — Bernard Beauzamy</a></li>
-            <li><a href="https://www.stripe.press/poor-charlies-almanack/talk-two?progress=97.43%" target="_blank">Charlie Munger Q&A — Poor Charlie's Almanack</a></li>
+            <li><a href="https://www.maths.tcd.ie/pub/ims/bull48/M4801.pdf" target="_blank">Real Life Mathematics</a> — Bernard Beauzamy</li>
+            <li><a href="https://www.stripe.press/poor-charlies-almanack/talk-two?progress=97.43%" target="_blank">Charlie Munger Q&A</a> — Poor Charlie's Almanack</li>
             <li><a href="https://energyandhumanity.com/" target="_blank">Energy and Humanity</a></li>
-            <li><a href="https://caseyhandmer.wordpress.com/2025/12/03/scaling-career-and-family-systems-thinking-public-school-home-enrichment/" target="_blank">Scaling Career and Family — Casey Handmer</a></li>
-            <li><a href="https://dirtcheapstocks.substack.com/p/how-warren-buffett-earned-a-39-irr" target="_blank">How Warren Buffett Earned a 39% IRR — Buffett Knows Arb</a></li>
-            <li><a href="https://moontowermeta.com/" target="_blank">Moontower — Kris Abdelmessih</a></li>
+            <li><a href="https://caseyhandmer.wordpress.com/2025/12/03/scaling-career-and-family-systems-thinking-public-school-home-enrichment/" target="_blank">Scaling Career and Family</a> — Casey Handmer</li>
+            <li><a href="https://dirtcheapstocks.substack.com/p/how-warren-buffett-earned-a-39-irr" target="_blank">How Warren Buffett Earned a 39% IRR</a> — Buffett Knows Arb</li>
+            <li><a href="https://moontowermeta.com/" target="_blank">Moontower</a> — Kris Abdelmessih</li>
           </ul>
         </div>
       </main>
