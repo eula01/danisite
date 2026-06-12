f133faf3ffd0f4624ac40c8dabd2609f38a697dd
Daniela Dobrinov <dani@MacBook-Air-8.local>
2026-06-12 14:45:45 +0100

    Add Moontower link to links page

--- pages/links.tsx
+++ pages/links.tsx
@@ -7,6 +7,12 @@ export default function Links() {
         <div>
           <p>A random selection of underpriced writings / links</p>
           <ul>
+            <li><a href="https://www.maths.tcd.ie/pub/ims/bull48/M4801.pdf" target="_blank">Real Life Mathematics — Bernard Beauzamy</a></li>
+            <li><a href="https://www.stripe.press/poor-charlies-almanack/talk-two?progress=97.43%" target="_blank">Charlie Munger Q&A — Poor Charlie's Almanack</a></li>
+            <li><a href="https://energyandhumanity.com/" target="_blank">Energy and Humanity</a></li>
+            <li><a href="https://caseyhandmer.wordpress.com/2025/12/03/scaling-career-and-family-systems-thinking-public-school-home-enrichment/" target="_blank">Scaling Career and Family — Casey Handmer</a></li>
+            <li><a href="https://dirtcheapstocks.substack.com/p/how-warren-buffett-earned-a-39-irr" target="_blank">How Warren Buffett Earned a 39% IRR — Buffett Knows Arb</a></li>
+            <li><a href="https://moontowermeta.com/" target="_blank">Moontower — Kris Abdelmessih</a></li>
           </ul>
         </div>
       </main>
